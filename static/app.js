aa2721142c99e4b15d3aa904835d23d17ced24b1
leah-handel <leah.handel@gmail.com>
2021-04-02 13:38:19 -0500

    made reindexing function, data ready to use

--- static/app.js
+++ static/app.js
@@ -1,20 +1,35 @@
-function unpack(rows, index) {
-    return rows.map(function(row) {
-      return row[index];
-    });
-  }
+function reindex(rawData, newObject) {
+  rawData.forEach(function(subject){
+    var id = subject.id;
+    newObject[id] = subject;
+  });
+  console.log(newObject);
+};
 
+// making the data easier to get into
 
 d3.json("static/samples.json").then(function(data) {
     console.log(data);
     var metadata = data.metadata;
+    var demographics = {};
     //var ids = metadata.map(subject => subject.id);
     //console.log(ids);
-    var demographics = {};
-    metadata.forEach(function(subject){
-        var id = subject.id;
-        demographics[id] = subject;
-    })
-    console.log(demographics);
-});
+    
+   //metadata.forEach(function(subject){
+    //    var id = subject.id;
+     //   demographics[id] = subject;
+    //});
+    //console.log(demographics);
+    //console.log(demographics[940]["ethnicity"]); this worked
+
+    var sampleData =  data.samples;
+    var samples = {};
+
+    reindex(metadata, demographics);
+    reindex(sampleData, samples);
+
+    });
+
+
+
 
